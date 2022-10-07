7a1e4ac9b71b6bfc1973c2c4428c89bf07ca4e75
Jakob McPherson <jakob@mcpherson.org>
2022-10-07 12:46:09 -0500

    first commit

---
+++ main.swift
@@ -0,0 +1,27 @@
+var strings: [String] = []
+
+while let line = readLine() {
+    strings.append(line)
+}
+
+for i in 0 ..< strings.count {
+    let cur = strings[i]
+    var comp = cur
+    var j = i - 1
+    
+    if j != -1 {
+        comp = strings[j]
+    }
+    
+    while comp > cur {
+        strings[j] = cur
+        strings[j + 1] = comp
+        j -= 1
+        
+        if j != -1 {
+            comp = strings[j]
+        } else {
+            break
+        }
+    }
+}   
